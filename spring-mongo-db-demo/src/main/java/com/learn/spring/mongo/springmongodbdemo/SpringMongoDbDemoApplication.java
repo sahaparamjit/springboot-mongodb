0723dd165bc897d4f8611f304f86f32da45542cc
Paramjit Saha <sahaparamjit@gmail.com>
2021-03-27 10:47:16 +0000

    Adding basic spring boot app

---
+++ spring-mongo-db-demo/src/main/java/com/learn/spring/mongo/springmongodbdemo/SpringMongoDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.mongo.springmongodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbDemoApplication.class, args);
+	}
+
+}
---
+++ spring-mongo-db-demo/src/test/java/com/learn/spring/mongo/springmongodbdemo/SpringMongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.mongo.springmongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
