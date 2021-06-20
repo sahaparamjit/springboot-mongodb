1b65b27241a4ddce27560ee0fc4702ac8c1e6374
Ubuntu <ubuntu@ip-172-31-8-47.ap-south-1.compute.internal>
2021-06-20 11:34:21 +0000

    Decorator pattern added

--- spring-mongo-db-demo/src/main/java/com/learn/spring/mongo/springmongodbdemo/designpattern/Decorator.java
+++ spring-mongo-db-demo/src/main/java/com/learn/spring/mongo/springmongodbdemo/designpattern/Decorator.java
@@ -3,6 +3,77 @@ package com.learn.spring.mongo.springmongodbdemo.designpattern;
 
 public class Decorator {
     public static void main(String[] args) {
-         System.out.println("Hello world!");
+         CoffeeService espresso = new WithExtraSugar(new WithExtraMilk(new Espresso()));
+         System.out.println("Item : "+ espresso.getDescription() + " Total Cost: "+espresso.getCost());
+    }
+}
+
+/** 
+ * CoffeeService -> Espresso, Latte, 
+*/
+interface CoffeeService {
+    String getDescription();
+    double getCost();
+}
+
+class Espresso implements CoffeeService {
+
+
+    @Override
+    public String getDescription() {
+        return "Espresso";
+    }
+
+    @Override
+    public double getCost() {
+        return 1.99;
+    }
+}
+
+
+abstract class CoffeeDecorator implements CoffeeService{
+    protected CoffeeService coffeeService;
+
+    public CoffeeDecorator (final CoffeeService coffeeService) {
+        this.coffeeService = coffeeService;
+    }
+}
+
+
+class WithExtraSugar extends CoffeeDecorator {
+    
+
+    public WithExtraSugar(CoffeeService coffeeService) {
+        super(coffeeService);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Added extra sugar, " + coffeeService.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return 0.99 + coffeeService.getCost();
+    }
+}
+
+
+
+class WithExtraMilk extends CoffeeDecorator {
+    
+
+    public WithExtraMilk(CoffeeService coffeeService) {
+        super(coffeeService);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Added extra milk, " + coffeeService.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return 0.25 + coffeeService.getCost();
     }
 }
